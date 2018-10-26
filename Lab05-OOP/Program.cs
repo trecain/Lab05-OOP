02e8f7d90d73c88dd783835b6182a09a9896cd89
trecain <developedbycain@yahoo.com>
2018-10-26 12:59:59 -0500

    Add project files.

---
+++ Lab05-OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab05_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
